7147cfce6be2b080e33f72c8bd7c6231eabcbf9e
Khyati Gems <khyatipreciousgems@gmail.com>
2026-02-09 23:43:12 +0530

    fix: imagekit import and add next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,21 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
-  reactCompiler: true,
   images: {
     remotePatterns: [
       {
-        protocol: "https",
-        hostname: "res.cloudinary.com",
+        protocol: 'https',
+        hostname: 'res.cloudinary.com',
       },
       {
-        protocol: "https",
-        hostname: "placehold.co",
-      },
-      {
-        protocol: "https",
-        hostname: "cdn.khyatigems.com",
+        protocol: 'https',
+        hostname: 'ik.imagekit.io',
       },
     ],
   },
